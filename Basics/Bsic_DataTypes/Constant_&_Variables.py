f16bc563ec6dfb893a7fde9dee308fe4d9381cc9
Hitesh Nagar <74867491+hiteshnagar2611@users.noreply.github.com>
2025-08-19 16:09:19 +0000

    c

---
+++ Basics/Bsic_DataTypes/Constant_&_Variables.py
@@ -0,0 +1,15 @@
+#variables -> contenars that store some value and can be modified
+#constant -> one created value can not be modified
+
+superhero = "Iron Man"
+print(superhero)
+
+hero1,hero2,hero3 = "Iron Man","Spider Man","Bat Man"
+print(hero1)
+print(hero2)
+print(hero3)
+
+x=y=z=12
+print(x)
+print(y)
+print(z)
