20600901155335cc2279ee5ab896c154a40eaa8c
Hitesh Nagar <74867491+hiteshnagar2611@users.noreply.github.com>
2025-08-19 16:50:06 +0000

    c

---
+++ Basics/Objects/Set.py
@@ -0,0 +1,16 @@
+# Curly Brackets
+
+my_set ={1,2,3,4,5}
+print(my_set)
+
+#set does not support indexing 
+#print(my_set[2]) #throw error
+
+#convert a list to a set
+list = [1,2,3,4,5,6]
+x = set(list)
+print(x)
+
+#set does not allows duplicate values
+set_3 ={1,2,2,3,4,4}
+print(set_3)
