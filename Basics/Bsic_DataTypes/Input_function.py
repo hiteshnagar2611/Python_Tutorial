472492ca225ac6487edcaeaae63e33bb00f4a5ca
Hitesh Nagar <74867491+hiteshnagar2611@users.noreply.github.com>
2025-08-19 16:17:19 +0000

    c

---
+++ Basics/Bsic_DataTypes/Input_function.py
@@ -0,0 +1,21 @@
+#user gives input 
+#it assumes that the given input is string
+
+number_1 = input("Enter number: ") #12
+number_2 = input("Enter number: ") #12
+sum = number_1 + number_2
+print(sum) # output -> 1212
+
+number_11 = int(input("Enter number: ")) #12
+number_22 = int(input("Enter number: ")) #12
+sum2 = number_11 + number_22
+print(sum2) #output -> 24
+
+
+#Changing Data Type 
+num = 5
+print(num)
+type(num)
+float(num)
+print(num)
+type(num)
