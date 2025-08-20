09024b7dc6b944b2cf1c270b6b244698fddf203c
Hitesh Nagar <74867491+hiteshnagar2611@users.noreply.github.com>
2025-08-20 17:49:42 +0000

    c

---
+++ Basics/Condition_Statment/If_else.py
@@ -0,0 +1,15 @@
+a = 10
+b = 20
+
+if a>b:
+    print("a is the gretest number")
+else:
+    print("b is the gretest number")
+
+
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+if x>y:
+    print("x is the grestest number")
+else:
+    print("y is the grestest number")
---
+++ Basics/Operators/Membership_operator.py
@@ -0,0 +1,11 @@
+# in    not in 
+
+a = 5
+b = 10
+c = [1,2,3,4,5,6]
+
+print(a in c) #True
+print(b in c) # False
+
+print(a not in c) # False
+print(b not in c) # True
