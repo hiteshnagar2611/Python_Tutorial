0c766a222d9865b67f7ac524f151cee6151a5dae
Hitesh Nagar <74867491+hiteshnagar2611@users.noreply.github.com>
2025-08-21 17:03:59 +0000

    c

---
+++ NumPy/NumPy_Array/Analysing_a_numpy_array.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+c = np.random.randint(10,90,(5,5))
+print(c)
+
+#array dimensions
+print(c.shape)
+
+#number of dimensions
+print(c.ndim)
+
+#number of elements in array
+print(c.size)
+
+#checking the datatype of values in the array
+print(c.dtype)
+
